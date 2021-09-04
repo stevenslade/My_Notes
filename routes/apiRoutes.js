e182d24e6de11430575f993e75877243fc0af2e9
Ben S <benslinde@gmail.com>
2021-09-04 18:17:16 -0400

    route files added

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    store
+    .getNotes()
+    .then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-//const apiRoutes = require('./routes/apiRoutes');
-//const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 // allows for use of public directory, need an index.html file in public folder
 app.use(express.static('public'));
-//app.use('/api', apiRoutes);
-//app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
