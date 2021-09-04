c2c09f0f1eac0701f34ff226398d2d1c683c0ba8
Ben S <benslinde@gmail.com>
2021-09-04 17:40:30 -0400

    WIP

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+//const apiRoutes = require('./routes/apiRoutes');
+//const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+// allows for use of public directory, need an index.html file in public folder
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+//app.use('/api', apiRoutes);
+//app.use('/', htmlRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
