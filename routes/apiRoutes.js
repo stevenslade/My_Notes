9a71cf3b9636a16c44adea364efe69fd51f1fe2b
Ben S <benslinde@gmail.com>
2021-09-04 20:54:29 -0400

    added get routes in server.js, added clog middleware app.use

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,4 +10,6 @@ router.get('/notes', (req, res) => {
 });
 
 
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const { clog } = require('./middleware/clog');
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -11,5 +14,18 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+app.use(clog);
+
+//GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//unlike activity 26 I do not need a feedback route
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
